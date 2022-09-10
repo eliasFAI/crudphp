54d2606323539e46fb0bd0c76d30c5ed1e85ea49
eliasFAI <elias.diaz@est.fi.uncoma.edu.ar>
2022-09-09 21:04:03 -0300

    aprendiendo crear crud en php

---
+++ includes/header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP MYSQL CRUD</title>
+    <!-- BOOSTRAP 5 -->
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css"
+ rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" 
+ crossorigin="anonymous"> 
+
+ <!-- FONT AWESOME 5 -->
+ <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+  </head>
+  <body>
+    <nav class="navbar navbar-dark bg-dark">
+      <div class="container">
+        <a class="navbar-brand" href="index.php">PHP MySQL CRUD</a>
+      </div>
+    </nav>
+
+</head>
+<body>
+    <nav class="navbar navbar-dark bg-dark">
+        <div class="container">
+           <a href="index.php" class="navbar-brand">PHP MYSQL CRUD</a>
+        </div>
+    </nav>
